9633fe94951ba13435195eadced2366808b7ed1c
Darsh-Turakhia-GNU <56975912+Darsh1561@users.noreply.github.com>
2021-06-20 09:07:19 +0530

    Update cric.cpp

--- cric.cpp
+++ cric.cpp
@@ -1,8 +1,3 @@
-/*
-This is just a basic version of cricket scorecard it just accepts the runs scored off a ball by batting team and then adds it up to the total in both the innings and declares the winner
-The player names are just for fun and is not displayed anywhere
-But as you know some bugs are left purposefully
-*/
 #include "iostream"
 #include "cstring"
 #include "stdlib.h"
